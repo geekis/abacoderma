c93fd9e155deffdfc183976c4341f0233ab33608
Egill Heinesen <egill@geek.is>
2025-08-19 13:23:59 +0000

    Fix Hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
+import { Content, isFilled } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
 /**
@@ -11,7 +11,12 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
-  console.log(slice.primary.url.link)
+
+  const bgUrl = isFilled.image(slice.primary.image)
+      ? slice.primary.image.url
+      : undefined;
+
+
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -25,16 +30,19 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       <div className="grid gap-4 col-start-2 col-span-5 self-center">
         <h1 className="font-serif text-slate-50 text-5xl">{slice.primary.title}</h1>
         <p className="text-slate-50">{slice.primary.text}</p>
-        <div className="mt-4">
-          <PrismicNextLink
-              field={slice.primary.url}
-              className="border border-slate-50 rounded-3xl px-6 py-3 text-slate-50 hover:bg-slate-50 hover:text-slate-700 ease-in-out duration-200"
-          >
-            {slice.primary.url.text || "Read more"}
-          </PrismicNextLink>
-        </div>
 
-
+        {isFilled.link(slice.primary.url) && (
+            <div className="mt-4">
+              <div className="mt-4">
+                <PrismicNextLink
+                    field={slice.primary.url}
+                    className="border border-slate-50 rounded-3xl px-6 py-3 text-slate-50 hover:bg-slate-50 hover:text-slate-700 ease-in-out duration-200"
+                >
+                  {slice.primary.url.text || "Read more"}
+                </PrismicNextLink>
+              </div>
+            </div>
+        )}
       </div>
     </section>
   );
