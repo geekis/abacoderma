86eab1e41a1f467b5cb92f9608e80e7759b18a5e
Egill Heinesen <egill@geek.is>
2026-01-15 18:30:31 +0000

    Mac to vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+          <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96"/>
+          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>
+          <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+          <meta name="apple-mobile-web-app-title" content="AbacoDerma"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+      </head>
       <body
-        className={`${libreBaskerville.variable} ${geistMono.variable} antialiased max-w-7xl mx-3 2xl:m-auto`}
+          className={`${libreBaskerville.variable} ${geistMono.variable} antialiased max-w-7xl mx-3 2xl:m-auto`}
       >
-        {children}
+      {children}
       </body>
-    </html>
+      </html>
   );
 }
--- src/slices/Boka/index.tsx
+++ src/slices/Boka/index.tsx
@@ -12,40 +12,18 @@ export type BokaProps = SliceComponentProps<Content.BokaSlice>;
  */
 const Boka: FC<BokaProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for boka (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <section
+          data-slice-type={slice.slice_type}
+          data-slice-variation={slice.variation}
+      >
+        <iframe
+            src="https://noona.app/abacoderma/book?iframe=true&darkModeDisabled=true&showCancelButton=true"
+            frameBorder="0"
+            width="100%"
+            height="1750"
+
+        ></iframe>
+      </section>
   );
 };
 
